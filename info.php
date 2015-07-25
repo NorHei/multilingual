664757622bb232d86acea594601aaf6934002b09
NorHei <norbert@heimsath.org>
2015-07-26 00:05:41 +0200

    set Version

--- info.php
+++ info.php
@@ -20,13 +20,13 @@ if(count(get_included_files()) ==1){$z="HTTP/1.0 404 Not Found";header($z);die($
 
 
 $module_directory = 'mod_multilingual';
-$module_name = 'Multilingual Switcher 1.6.6';
+$module_name = 'Multilingual Switcher 1.6.7';
 $module_function = 'snippet';
-$module_version = '1.6.6';
+$module_version = '1.6.7';
 $module_status	= 'alpha';
 $module_platform = '2.8.3';
-$module_author = 'LuiseHahne, NorHei';
+$module_author = 'LuiseHahne, Norbert Heimsath';
 $module_license = 'GNU General Public License 2';
-$module_requirements = 'min. PHP 5.2.2 and WB 2.8.3 or higher)';
-$module_description = 'This snippet switches between different languagesm and tries to keep position in page structure';
+$module_requirements = 'min. PHP 5.2.2 and WB 2.8.2 or higher)';
+$module_description = 'This snippet switches between different languages and tries to keep position in page structure';
 
