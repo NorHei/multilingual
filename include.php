35f535deca1ed9741f0f7199179d6e49a64abf43
NorHei <norbert@heimsath.org>
2017-01-31 18:21:24 +0100

    Added function for Googles hreflang

--- include.php
+++ include.php
@@ -45,5 +45,36 @@ function language_array() {
     return $aList;
 }
 
+/**
+    @brief Display googles  hreflang links
 
+    @param bool $output Echo content on true , return content on false
+    @param bool $show_recent Show recent Page [true|false]
+    @param bool $show_x_default Show x-default [true|false]
+    @return string Returns generated lines only on $output===true
+
+*/
+function language_hreflang ($output=true, $show_recent=true, $show_x_default=true){
+    // Used for prefered Render type HTML5 or XHTML ... 
+    $end_tag=" /";  
+    if (defined('WB_RENDER')) $Render=WB_RENDER;
+    if ($this->Render!="xhtml") $end_tag=" ";
+
+    // output var
+    $out="\n";
+    
+    if ($show_x_default==true) {
+        $out.="\t\t".'<link rel="alternate" hreflang="x-default" href="'.WB_URL.'" '.$end_tag.'>'.PHP_EOL;
+    }
+    
+    // Lets walk the language array 
+    $aLangLinks = language_array();
+	foreach($aLangLinks as $key=>$link){
+        // If recent Language schould not be linked
+		if($show_recent!==true AND LANGUAGE == $key ) continue; 
+		$out.="\t\t".'<link rel="alternate" hreflang="'.strtolower($key).'" href="'.$link['url'].'" '.$end_tag.'>'.PHP_EOL;
+	}
+    if ($output===true) echo $out;
+    else                return $out;
+}
 
